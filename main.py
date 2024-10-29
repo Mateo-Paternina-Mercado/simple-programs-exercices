e2181038b5a183e6ea1348f951844ffbf4cc8f8b
Mateo-Paternina-Mercado <185979424+Mateo-Paternina-Mercado@users.noreply.github.com>
2024-10-29 15:25:11 -0500

    xd

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-#Escriba un programa que pida al usuario que escriba su nombre, y lo salude llamándolo por su nombre.
-
-#Ingrese su nombre: Perico
-#Hola, Perico
-
-name = input("Ingrese su nombre: ")
-message =  f"Hola, {name}"
-print(message)
+xd
