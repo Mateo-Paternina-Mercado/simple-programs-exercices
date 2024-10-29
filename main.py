4055ed813e5e32dddacef78ed2ef18ffe83353cf
Mateo-Paternina-Mercado <185979424+Mateo-Paternina-Mercado@users.noreply.github.com>
2024-10-29 15:27:38 -0500

    Recuperacion ejercicio de saludo

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-xd
+#Escriba un programa que pida al usuario que escriba su nombre, y lo salude llamándolo por su nombre.
+
+#Ingrese su nombre: Perico
+#Hola, Perico
+
+name = input("Ingrese su nombre: ")
+message =  f"Hola, {name}"
+print(message)
